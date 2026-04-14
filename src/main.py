6d1194bb5e7b7af6e568bda061dc536fa932de3e
KirtanG <kirtangoswami97@gmail.com>
2026-04-14 23:07:15 +0530

    Added Dependencies and Tested FastAPI Server

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ticket-triage!")
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return HTMLResponse(
+        content="<h1>The Server is running!<h1>"
+    )
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app=app,host="0.0.0.0",port=8080)
